e7f031261a6e269e490eb965877b65e47dc7bc26
thegenggo <kritsanaphong-keang@hotmail.com>
2021-02-09 16:36:37 +0700

    All files completed

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char* &x,int N){
+	x = new char[N];
+	for(int i = 0; i < N;i++) x[i] = 'A'+i;
+	cout << x;
+	*(x+N) = 0;
 }
 
 int main(){
@@ -13,7 +14,7 @@ int main(){
 	cout << "N = ";
 	cin >> N;		
 	myString(p,N);
-	cout << p;	
+	cout << p;
 	delete [] p;
 	return 0;
 }
